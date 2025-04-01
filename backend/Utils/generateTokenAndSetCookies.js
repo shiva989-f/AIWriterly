0edb77757cc3c2a9948ff1cbf29fd3099a014104
shiva989-f <kumarshiva05722@gmail.com>
2025-04-02 01:24:23 +0530

    Finish

--- backend/Utils/generateTokenAndSetCookies.js
+++ backend/Utils/generateTokenAndSetCookies.js
@@ -11,7 +11,8 @@ export const generateTokenAndSetCookies = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true, // accessible by only http not with js to prevent from xss
     secure: process.env.NODE_ENV === "production", // Secure = true only when we are in production means (https) else in development secure = false (http)
-    sameSite: "strict", // prevent csrf
+    // sameSite: "strict", // prevent csrf
+    sameSite: "None", // because frontend and backend hosted on different domains
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7days in the format of ms
   });
   return token;
